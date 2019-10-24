d7af67285d61fba9cd7ac3ae5b4244d4ffbd268d
kingntop <kingntop@naver.com>
2019-10-24 18:25:55 +0900

    1111

---
+++ main.js
@@ -0,0 +1,5 @@
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + 'https://www.bloter.net/archives/358655';
+    }
+});
